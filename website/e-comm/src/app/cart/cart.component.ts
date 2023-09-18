e77b64ae2733f8e856b7cac3e5325f79d727b582
Peter Debevec <peterusdebevec@gmail.com>
2023-09-18 22:06:19 +0200

    update

--- website/e-comm/src/app/cart/cart.component.ts
+++ website/e-comm/src/app/cart/cart.component.ts
@@ -18,6 +18,19 @@ export class CartComponent {
 
   constructor(private productsService: ProductsService){}
 
+  qunatityChange(productId: string): void{
+
+  }
+
+  removeProduct(productId: string): void{
+    this.cartitems.splice(
+      this.cartitems.findIndex((element: any) => {
+        return element.title == productId
+    }), 1)
+    localStorage.setItem('cart', JSON.stringify(this.cartitems))
+    window.location.reload()
+  }
+
   ngOnInit(): void{
     this.cartitems = JSON.parse(localStorage['cart'])
     this.productsService.getProducts().subscribe((products) => {
