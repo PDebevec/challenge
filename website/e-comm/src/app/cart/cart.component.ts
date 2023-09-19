94c431610997d72b46d63fc994f45f24ebda2ba1
Peter Debevec <peterusdebevec@gmail.com>
2023-09-19 21:09:43 +0200

    funkcionalnost

--- website/e-comm/src/app/cart/cart.component.ts
+++ website/e-comm/src/app/cart/cart.component.ts
@@ -2,24 +2,38 @@ import { Component } from '@angular/core';
 import { ProductsService } from '../products.service'
 import { Product } from '../product.interface'
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule
   ],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-  cartitems: any;
-  products: Product[] = [];
+  cartitems: any[] = [];
+  products: any[] = [];
+  cartTotal: number = 0;
 
   constructor(private productsService: ProductsService){}
 
-  qunatityChange(productId: string): void{
+  qunatityChange(product: any): void{
+    let index = this.cartitems.findIndex((element: any) => {
+      return element.title == product[0].cardTitle
+    })
+    
+    this.products[index][1].quantity = product[1].quantity
 
+    localStorage.setItem('cart', JSON.stringify(this.cartitems))
+
+    this.cartTotal = 0
+    this.products.forEach(element => {
+      this.cartTotal += element[0].listOfPlans[0].price.amount * element[1].quantity / 100
+    })
   }
 
   removeProduct(productId: string): void{
@@ -37,7 +51,8 @@ export class CartComponent {
       products.forEach(element => {
         this.cartitems.forEach((item: any) => {
           if(element.cardTitle == item.title){
-            this.products.push(element)
+            this.products.unshift([element, item])
+            this.cartTotal += element.listOfPlans[0].price.amount * item.quantity / 100;
           }
         });
       });
